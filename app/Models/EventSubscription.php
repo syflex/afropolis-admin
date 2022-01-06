1c402b5ba0cd0b7336abf7252518c6aa2c2fbec8
gaiyadev <gaiyaobed94@gmail.com>
2022-01-07 00:14:07 +0100

    added isSubcribe column to event_subscribers

--- app/Models/EventSubscription.php
+++ app/Models/EventSubscription.php
@@ -13,7 +13,7 @@ class EventSubscription extends Model
      *
      * @var string
      */
-    protected $table = 'event_subscribes';
+    protected $table = 'event_subscribers';
 
     /**
      * The attributes that are mass assignable.
---
+++ database/migrations/2022_01_06_231141_add_is_subscribe_to_event_subscribers.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddIsSubscribeToEventSubscribers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('event_subscribers', function (Blueprint $table) {
+                        $table->boolean('isSubscribe');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('event_subscribers', function (Blueprint $table) {
+            //
+        });
+    }
+}
