97e7a88b9293a316ca217780b3c4308daacc2598
gaiyadev <gaiyaobed94@gmail.com>
2022-01-07 20:20:21 +0100

    tiny fixed

--- database/seeders/CollectionSeeder.php
+++ database/seeders/CollectionSeeder.php
@@ -15,31 +15,31 @@ class CollectionSeeder extends Seeder
     public function run()
     {
         $collection = new Collection;
-        $collection->user_id = 1; //The official afropolis
+        $collection->user_id = 1; 
         $collection->name = "testing collection";
         $collection->description = "the official afropolis";
         $collection->image = '1.jpg';
         $collection->save();
 
         $collection = new Collection;
-        $collection->user_id = 1; //The official afropolis
+        $collection->user_id = 1; 
         $collection->name = "testing";
         $collection->description = "the official afropolis";
-        $collection->image = '1.jpg';
+        $collection->image = '2.jpg';
         $collection->save();
 
         $collection = new Collection;
-        $collection->user_id = 1; //The official afropolis
+        $collection->user_id = 1; 
         $collection->name = "teser";
         $collection->description = "the official afropolis";
-        $collection->image = '1.jpg';
+        $collection->image = '2.jpg';
         $collection->save();
 
         $collection = new Collection;
-        $collection->user_id = 1; //The official afropolis
+        $collection->user_id = 1; 
         $collection->name = "test";
         $collection->description = "the official afropolis";
-        $collection->image = '1.jpg';
+        $collection->image = '3.jpg';
         $collection->save();
     }
 }
