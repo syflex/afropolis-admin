c6cb5fa25fe738fe352d6bae7a91cc7df08dbb6d
Simon Onazi Moses <syflex360@gmail.com>
2022-01-16 17:50:21 +0100

    updated admin

--- app/Notifications/AuthNotification.php
+++ app/Notifications/AuthNotification.php
@@ -29,7 +29,7 @@ class AuthNotification extends Notification
      */
     public function via($notifiable)
     {
-        return ['mail'];
+        return ['database'];
     }
 
     /**
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -19,7 +19,7 @@ class UserSeeder extends Seeder
             'name' => 'Afropolis',
             'slug' => 'Afropolis',
             'email' => 'afropolistech@gmail.com',
-            'password' =>  Hash::make('secret'),
+            'password' =>  Hash::make('Technical2021'),
             'phone' => '2347088886806',
             'is_admin' => true,
             'active' => true,
@@ -30,6 +30,42 @@ class UserSeeder extends Seeder
             'slug' => 'joseeph-adeleke',
             'email' => 'josephadeleke1914@gmail.com',
             'password' =>  Hash::make('Qwerty1'),
+            'is_admin' => true,
+            'active' => true,
+        ]);
+
+        User::create([
+            'name' => 'Olatunde Obajeun',
+            'slug' => 'oba',
+            'email' => 'Olatundeobajeun@gmail.com',
+            'password' =>  Hash::make('Technical2021'),
+            'is_admin' => true,
+            'active' => true,
+        ]);
+
+        User::create([
+            'name' => 'Joshua Akubo Gabriel',
+            'slug' => 'josh',
+            'email' => 'Joshuaakubogabriel@gmail.com',
+            'password' =>  Hash::make('Technical2021'),
+            'is_admin' => true,
+            'active' => true,
+        ]);
+
+        User::create([
+            'name' => 'Qudus',
+            'slug' => 'qudus',
+            'email' => 'qudus@qdancecenter.com',
+            'password' =>  Hash::make('Technical2021'),
+            'is_admin' => true,
+            'active' => true,
+        ]);
+
+        User::create([
+            'name' => 'Oluwaseun Adebayo',
+            'slug' => 'osu',
+            'email' => 'syflex360@gmail.com',
+            'password' =>  Hash::make('Simon@360'),
             'is_admin' => true,
             'active' => true,
         ]);
